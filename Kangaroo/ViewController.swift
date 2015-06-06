cb033fc3c5ab5a65976cb65d79bde631304393f7
Jack Cook <jack@jackcook.nyc>
2015-06-06 11:52:21 -0400

    Initial Commit

---
+++ Kangaroo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kangaroo
+//
+//  Created by Jack Cook on 6/6/15.
+//  Copyright (c) 2015 Jack Cook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
