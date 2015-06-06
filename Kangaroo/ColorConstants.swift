4fa80ee3b29ddb40c270f6fd05af30215a495f6e
Jack Cook <jack@jackcook.nyc>
2015-06-06 16:18:01 -0400

    Added loading screen

---
+++ Kangaroo/ColorConstants.swift
@@ -0,0 +1,16 @@
+//
+//  ColorConstants.swift
+//  Kangaroo
+//
+//  Created by Jack Cook on 6/6/15.
+//  Copyright (c) 2015 Jack Cook. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class func mainKangarooColor() -> UIColor {
+        return UIColor(red: 236/255, green: 135/255, blue: 19/255, alpha: 1)
+    }
+}
---
+++ Kangaroo/LoadingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingViewController.swift
+//  Kangaroo
+//
+//  Created by Jack Cook on 6/6/15.
+//  Copyright (c) 2015 Jack Cook. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoadingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
